c8bc44ca5bae48e13f6949baa4baa8ec322cd4fd
Антон Ханджян <anton.hand@mail.ru>
2020-11-21 03:44:36 +0300

    09 done

---
+++ 09_I18nL10n/num_guess.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+#include <libintl.h>
+#include <locale.h>
+
+#define _(STRING) gettext(STRING)
+#define LOCALE_PATH "."
+
+int main(){
+    setlocale (LC_ALL, "");
+	bindtextdomain ("num_guess", LOCALE_PATH);
+	textdomain ("num_guess");
+
+    int lower = 0;
+    int upper = 100;
+
+    int is_guess = 0;
+
+    printf(_("Guess a number from %d to %d\n"), lower, upper);
+
+    do {
+        printf(_("Is your number greater than %d?\n"), (upper + lower) / 2);
+        char answer[100];
+        fgets(answer, sizeof(answer), stdin);
+
+        if(strcasecmp(answer, _("yes\n")) == 0){
+            lower = (upper + lower) / 2 + 1;
+        } else if (strcasecmp(answer, _("no\n")) == 0){
+            upper = (upper + lower) / 2;
+        } 
+    } while(lower < upper);
+    printf(_("Your number is %d\n"), lower);
+
+    return 0;
+}
